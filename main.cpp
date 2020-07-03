9cd617eb4949dc44332a5ab812a0f276e88558eb
Stefan Su <stefansu28@gmail.com>
2020-07-02 21:10:52 -0400

    made pulse function which outputs sine waves

--- main.cpp
+++ main.cpp
@@ -2,10 +2,28 @@
 #include <cmath>
 
 #define PI 3.14159265
+#define SAMPLE_RATE 44100
+
+void pulse(FILE *file, int note, float duration) {
+    // convert the note to a frequency
+    // 0 is C4
+    // 440 is A4
+    float freq = 440 * exp2((float)(note + 3) /12);
+
+    float *buffer = NULL;
+    int numSamples = (int) (duration * SAMPLE_RATE);
+    buffer = (float *) malloc(sizeof(float) * numSamples);
+    for (int i = 0; i < numSamples; i++) {
+        buffer[i] = sin(freq*(2*PI) * i / SAMPLE_RATE);
+        // printf("%f\n", buffer[i]);
+    }
+
+    fwrite(buffer, sizeof(float), numSamples, file);
+
+    free(buffer);
+}
 
 int main(int argc, const char **argv) {
-    const int numSteps = 100;
-
     // the output file, this is where we will dump the raw audio
     FILE *output = NULL;
 
@@ -14,13 +32,13 @@ int main(int argc, const char **argv) {
         return 1;
     }
 
-    float *buffer = NULL;
-    buffer = (float *) malloc(sizeof(float) * numSteps);
-    for (int i = 0; i < numSteps; i++) {
-        buffer[i] = sin(2*PI * i/numSteps);
-    }
+    int notes[] = {
+        0, 2, 4, 5, 7, 9, 11, 12
+    };
 
-    fwrite(buffer, sizeof(float), numSteps, output);
+    for (int n = 0; n < sizeof(notes)/sizeof(int); n++) {
+        pulse(output, notes[n], .5);
+    }
 
     fclose(output);
     return 0;
